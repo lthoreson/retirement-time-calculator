6cde4f5bb5a3cd15fd97902dba60275d3b346024
Logan Thoreson <38727090+lthoreson@users.noreply.github.com>
2022-09-14 13:43:17 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -13,7 +13,8 @@ class Presentational extends React.Component {
       salary: 50000, // user input for salary variable
       save: 818, // monthly saving rate variable
       retire: 30, // time until retirement variable
-      balance: 0 };
+      balance: 0,
+      step: 0 };
 
 
     // bind methods so they can be called in JSX
@@ -35,26 +36,39 @@ class Presentational extends React.Component {
         timeline: 'month',
         save: Math.round(state.save * 12) }));
 
+    } else if (event.target.id === 'back' && this.state.step > 0) {
+      this.setState(state => ({
+        step: state.step - 1 }));
+
+    } else if (event.target.id === 'forward' && this.state.step < 3) {
+      this.setState(state => ({
+        step: state.step + 1 }));
+
     }
   }
 
-  // take changes to input fields, by input ID, and update the variables respectively
+  // update the variables whenever inputs change
   handleChange(event) {
-    if (event.target.id === 'salary') {
-      const salary = Number(event.target.value.replace(/,/g, ''));
-      if (!isNaN(salary)) {
-        this.setState({ salary: salary }); // convert string input to number and update salary variable to input
-      }
-    } else if (event.target.id === 'save') {
-      this.setState({ save: event.target.value }); // update saving rate variable to input
-    } else if (event.target.id === 'retire') {
-      this.setState({ retire: event.target.value }); // update retirement date variable to input
-    } else if (event.target.id === 'balance') {
-      const balance = Number(event.target.value.replace(/,/g, ''));
-      if (!isNaN(balance)) {
-        this.setState({ balance: balance });
-      }
-    }
+    switch (event.target.id) {
+      case 'salary':
+        const salary = Number(event.target.value.replace(/,/g, '')); // get salary input as number
+        if (!isNaN(salary)) {
+          this.setState({ salary: salary }); // update state
+        }
+        break;
+      case 'save':
+        this.setState({ save: event.target.value }); // update saving rate
+        break;
+      case 'retire':
+        this.setState({ retire: event.target.value }); // update retirement date
+        break;
+      case 'balance':
+        const balance = Number(event.target.value.replace(/,/g, '')); // get current balance input as number
+        if (!isNaN(balance)) {
+          this.setState({ balance: balance }); // update state
+        }
+        break;}
+
 
     // recalculates dependent variables based on the updated variable
     this.setState(state => {
@@ -64,29 +78,38 @@ class Presentational extends React.Component {
       }
 
       const rate = 0.07; // return on investment assumption
-      let yearSave = 12 * state.save; // convert saving rate to annual
-      const goal = 25 * (salary - yearSave); // multiply cost of living by 25
-      const time = Math.round(Math.log((goal + yearSave / rate) / (state.balance + yearSave / rate)) / rate); // update retire date
-      yearSave = Math.round((25 * salary - state.balance * Math.exp(state.retire * rate)) / ((Math.exp(state.retire * rate) - 1) / rate + 25) / 12); // update saving rate
+      let save = 12 * state.save; // convert saving rate to annual
+      const goal = 25 * (salary - save); // multiply cost of living by 25
 
       // choose formula based on calculator mode
-      if (!state.deadline) {
+      if (state.deadline) {
+        save = Math.round((25 * salary - state.balance * Math.exp(state.retire * rate)) / (
+        (Math.exp(state.retire * rate) - 1) / rate + 25) / 12); // update saving rate
+        return { save: save };
+      } else {
+        const time = Math.round(Math.log((goal + save / rate) / (
+        state.balance + save / rate)) / rate); // update retire date
         return { retire: time };
-      } else if (state.deadline) {
-        return { save: yearSave };
       }
     });
   }
 
   render() {
     // calculate monthly salary
-    let monthlySalary = this.state.salary / 12;
+    let monthlySalary = this.state.salary;
+    if (this.state.timeline === 'year') {// if salary is annual,
+      monthlySalary /= 12; // convert to monthly
+    }
     let year = 'selected';
     let month = '';
     let mode = 'Switch to Deadline Mode';
+    let message = `You'll have to invest 
+    ${this.state.save.toLocaleString('en-US')} per month 
+    to retire in 
+    ${this.state.retire} years. 
+    Your monthly allowance is ${Math.round(monthlySalary - this.state.save).toLocaleString('en-US')}`;
 
     if (this.state.timeline === 'month') {
-      monthlySalary = this.state.salary;
       year = '';
       month = 'selected';
     };
@@ -94,12 +117,10 @@ class Presentational extends React.Component {
       mode = 'Switch to Savings Mode';
     }
 
-
-
     // dictionary for input sections
     const inputs = {
 
-      // salary input section with annual & monthly toggle buttons
+      // salary input section with toggle between annual and monthly
       salary: /*#__PURE__*/
       React.createElement("div", null, /*#__PURE__*/
       React.createElement("div", null, /*#__PURE__*/
@@ -147,50 +168,37 @@ class Presentational extends React.Component {
 
 
       // button to toggle between calculator modes
-      switch: /*#__PURE__*/React.createElement("button", { id: "reverse", onClick: this.handleClick }, mode) };
+      switch: /*#__PURE__*/React.createElement("button", { id: "reverse", onClick: this.handleClick }, mode),
+
+      submit: /*#__PURE__*/
+      React.createElement("div", null, /*#__PURE__*/
+      React.createElement("button", { id: "back", onClick: this.handleClick, value: -1 }, "<"), /*#__PURE__*/
+      React.createElement("button", { id: "forward", onClick: this.handleClick, value: 1 }, ">")) };
+
 
 
     // dictionary for two possible result messages
-    const messages = {
-      retireYear: /*#__PURE__*/
-      React.createElement("div", null, /*#__PURE__*/
-      React.createElement("h1", null, "Results"), /*#__PURE__*/
-      React.createElement("div", { class: "bubble" }, /*#__PURE__*/
-      React.createElement("h2", null, "You'll have to work for ", /*#__PURE__*/React.createElement("br", null), this.state.retire, " more years.", /*#__PURE__*/React.createElement("br", null), "Your monthly allowance is ", Math.round(monthlySalary - this.state.save).toLocaleString('en-US')))),
-
-
-      retireSave: /*#__PURE__*/
-      React.createElement("div", null, /*#__PURE__*/
-      React.createElement("h1", null, "Results"), /*#__PURE__*/
-      React.createElement("div", { class: "bubble" }, /*#__PURE__*/
-      React.createElement("h2", null, "You'll have to invest ", this.state.save.toLocaleString('en-US'), " per month ", /*#__PURE__*/React.createElement("br", null), "to retire in ", this.state.retire, " years. ", /*#__PURE__*/React.createElement("br", null), "Your monthly allowance is ", Math.round(monthlySalary - this.state.save).toLocaleString('en-US')))) };
+    const results = /*#__PURE__*/
+    React.createElement("div", null, /*#__PURE__*/
+    React.createElement("h1", null, "Results"), /*#__PURE__*/
+    React.createElement("div", { class: "bubble" }, /*#__PURE__*/
+    React.createElement("h2", null, message)));
 
 
 
-
-    // element display order for calculator mode with savings rate input
-    const original = [
+    const itinerary = [
     inputs.salary,
     inputs.balance,
-    inputs.save,
-    inputs.switch,
-    messages.retireYear];
-
-
-    // element display order for calculator mode with retirement deadline input
-    const deadline = [
-    inputs.salary,
-    inputs.balance,
-    inputs.retire,
-    inputs.switch,
-    messages.retireSave];
+    [this.state.deadline ? inputs.retire : inputs.save, inputs.switch],
+    results];
 
 
     // choose elements to display based on calculator mode
     return /*#__PURE__*/(
       React.createElement("div", null, /*#__PURE__*/
       React.createElement("h1", null, "Financial Freedom Calculator"), /*#__PURE__*/React.createElement("hr", null),
-      this.state.deadline ? deadline : original));
+      itinerary[this.state.step],
+      inputs.submit));
 
 
   }}
